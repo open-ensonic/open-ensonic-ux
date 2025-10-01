878f421c293c532cf555681994fdd965c97ac2f0
jizhfang <jizhfang@gmail.com>
2025-10-01 14:05:22 +0800

    add index page

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,15 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: () => import('../components/HelloWorld.vue')
-    }
+      path: '/ux1',
+      name: 'ux1',
+      component: () => import('../views/ux1/Index.vue')
+    },
+      {
+          path: '/ux2',
+          name: 'ux2',
+          component: () => import('../views/ux2/Index.vue')
+      }
     // 可以在这里添加更多路由
   ]
 })
---
+++ src/views/ux1/Index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>ux1</div>
+</template>
+
+<style scoped>
+
+</style>
